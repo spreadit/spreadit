53e1514662ba1c96c8b12bbfb9e4f5b262f87418
Jett <whackashoe@gmail.com>
2014-08-22 22:53:31 -0500

    add norepeat to image thumbnailer

--- app/views/imagewrapper.blade.php
+++ app/views/imagewrapper.blade.php
@@ -4,6 +4,7 @@ body { min-width:100%; min-height:100%;
 @if (Input::has('url'))
     background-image:url({{ Input::get('url') }});
     background-size:cover;
+    background-repeat:no-repeat;
 @endif
 
 }
