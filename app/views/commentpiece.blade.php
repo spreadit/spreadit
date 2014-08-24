2e70a391307028361a90800dafaa79189e424013
Jett <whackashoe@gmail.com>
2014-08-24 04:40:53 -0500

    bugfix

--- app/views/commentpiece.blade.php
+++ app/views/commentpiece.blade.php
@@ -19,7 +19,7 @@
                 <input class="collapse" id="collapse-reply{{ $comment->id }}" type="checkbox">
                 <div class="replybox">
                     <form id="comment-form" method="post" class="flat-form flatpop-left">
-                        <input type="hidden" name="parent_id" value="{{ $comment->parent_id }}">
+                        <input type="hidden" name="parent_id" value="{{ $comment->id }}">
                         <p class="text">
                             <textarea name="data" id="data" placeholder="You have {{ (Comment::MAX_COMMENTS_PER_DAY - Comment::getCommentsInTimeoutRange()) }} of {{ Comment::MAX_COMMENTS_PER_DAY }} comments remaining ( per {{ Utility::prettyAgo(time() - Comment::MAX_COMMENTS_TIMEOUT_SECONDS) }})" maxlength="{{ Comment::MAX_MARKDOWN_LENGTH }}" required></textarea>
                         </p>
