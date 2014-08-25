650c5c0f6de4ea2c75501c93f8015e8c3ca6adf0
Jett <whackashoe@gmail.com>
2014-08-25 04:42:43 -0500

    add latex via mathjax, fixes #9

--- app/views/commonscripts.php
+++ app/views/commonscripts.php
@@ -1,1 +1,3 @@
 <script src="/assets/prod/build.min.js"></script>
+<script type="text/x-mathjax-config">MathJax.Hub.Config({ tex2jax: {inlineMath: [["$","$"],["\\(","\\)"]]} });</script>
+<script src="//cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
