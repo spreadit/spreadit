4e97c157ce32ff50f9c4d1c64ea43b8c79561472
Jett <whackashoe@gmail.com>
2014-09-04 22:22:55 -0500

    add days since request for info

--- app/views/threats.blade.php
+++ app/views/threats.blade.php
@@ -23,6 +23,17 @@
             </thead>
             <tbody>
                 <tr><td>Nothing has occurred so far, lets hope it doesn't</td></tr>
+                <tr><td>Just in case: It's been
+                <?php
+                $days_since_request = function() {
+                    $now = time();
+                    $your_date = strtotime("2014-01-01");
+                    $datediff = $now - $your_date;
+                    return floor($datediff / (60 * 60 * 24));
+                };
+                echo $days_since_request();
+                ?>
+                days since the last request for information</td></tr>
             </tbody>
         </table>
     </div>
