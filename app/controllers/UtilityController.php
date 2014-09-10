5590e6e86af17fd7283005e75eb85a5079483ba6
Jett <whackashoe@gmail.com>
2014-09-09 22:25:47 -0500

    remove unused code

--- app/controllers/UtilityController.php
+++ app/controllers/UtilityController.php
@@ -8,13 +8,6 @@ class UtilityController extends BaseController
         return View::make('imagewrapper');
     }
 
-    protected function generate_view($view)
-    {
-        $allowed_views = ['commentreplybox', 'commentsourcebox', 'commenteditbox', 'postreplybox', 'postsourcebox', 'posteditbox'];
-        if(!in_array($view, $allowed_views)) return "not allowed";
-        return View::make($view, Input::all());
-    }
-
     protected function titlefromurl()
     {
         $result = Input::has('url') ? Utility::titleFromUrl(Input::get('url')) : "url not given";
