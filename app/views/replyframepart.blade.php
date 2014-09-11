6c5504b7c30018c6033453a94c21228a8859fa12
Jett <whackashoe@gmail.com>
2014-09-11 13:43:57 -0500

    what should work.. but doesnt in ff

--- app/views/replyframepart.blade.php
+++ app/views/replyframepart.blade.php
@@ -1,7 +1,7 @@
 <?php
 $uuid = rand(0, 999999999);
 ?>
-<form class="reply-button-form">
+<form class="reply-button-form" action="/" method="post">
     <input type="hidden" name="post_id" value="{{ $post_id }}">
     <input type="hidden" name="parent_id" value="{{ $parent_id }}">
     <button type="submit" class="preview"
@@ -10,6 +10,7 @@ $uuid = rand(0, 999999999);
         formtarget="reply-box{{ $uuid }}">
         <label class="comment-action reply" for="collapse-reply{{ $uuid }}">reply </label>
     </button>
+    <button type="submit" style="display:none">hidden</button>
 </form>
 <input class="collapse" id="collapse-reply{{ $uuid }}" type="checkbox">
 <div class="replybox">
