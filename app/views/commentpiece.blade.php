d747e5809c2dc0f4b59c3e9e95a14c34436f1ce3
Jett <whackashoe@gmail.com>
2014-10-10 07:19:28 -0500

    dont follow votes (for crawlers)

--- app/views/commentpiece.blade.php
+++ app/views/commentpiece.blade.php
@@ -4,8 +4,8 @@
             <a class="username {{ UtilityController::anonymousClasses($comment) }}" rel="nofollow" href="{{ URL("/u/{$comment->username}") }}">{{ $comment->username }}</a>(<span class="upoints">{{ $comment->points }}</span>,<span class="uvotes">{{ $comment->votes }}</span>)
         </div>
         <div class="breaker comment-votes">
-            <a href="{{ URL::to("/vote/comment/" . $comment->id . "/up") }}" {{ UtilityController::bubbleText() }} class="vote {{ UtilityController::bubbleClasses($comment) }} {{ UtilityController::upvoteClasses($comment) }}" data-id="{{ $comment->id }}" data-type="comment" data-updown="up">&#x25B2;</a>
-            <a href="{{ URL::to("/vote/comment/" . $comment->id . "/down") }}" {{ UtilityController::bubbleText() }} class="vote {{ UtilityController::bubbleClasses($comment) }} {{ UtilityController::downvoteClasses($comment) }}" data-id="{{ $comment->id }}" data-type="comment" data-updown="down">&#x25BC;</a>
+            <a href="{{ URL::to("/vote/comment/" . $comment->id . "/up") }}" {{ UtilityController::bubbleText() }} class="vote {{ UtilityController::bubbleClasses($comment) }} {{ UtilityController::upvoteClasses($comment) }}" data-id="{{ $comment->id }}" data-type="comment" data-updown="up" rel="nofollow">&#x25B2;</a>
+            <a href="{{ URL::to("/vote/comment/" . $comment->id . "/down") }}" {{ UtilityController::bubbleText() }} class="vote {{ UtilityController::bubbleClasses($comment) }} {{ UtilityController::downvoteClasses($comment) }}" data-id="{{ $comment->id }}" data-type="comment" data-updown="down" rel="nofollow">&#x25BC;</a>
             <a href="{{ URL("/vote/comment/{$comment->id}") }}">
                 <span class="upvotes">{{ $comment->upvotes  }}</span>-<span class="downvotes">{{ $comment->downvotes }}</span> <span class="total-points">{{ ($comment->upvotes - $comment->downvotes) }}</span>
             </a>
