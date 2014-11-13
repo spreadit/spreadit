28eb1d77af5e407908de279b0ca510c5660776f9
Jett <whackashoe@gmail.com>
2014-11-13 04:56:52 -0600

    add post button on sidebar

--- app/views/shared/sidebar.blade.php
+++ app/views/shared/sidebar.blade.php
@@ -1,17 +1,21 @@
 @include ('shared.googlesitesearch')
 
 @if (Auth::check())
-    
+    <div class="add-post-shard">
+        <a href="/s/{{ $section->title }}/add"><button>add post</button></a>
+    </div>
 @else
-    <form id="login-form" action="{{ url('login') }}" method="post">
-        <p class="username">
-            <input name="username" type="username" value="" data-validation="nonempty username" id="username" placeholder="username" />
-        </p>
-        <p class="text">
-            <input name="password" type="password" data-validation="nonempty" id="password" placeholder="Password" />
-        </p>
-        <div class="submit">
-            <button type="submit">Login</button>
-        </div>
-    </form>
+    <div class="login-shard">
+        <form id="login-form" action="{{ url('login') }}" method="post">
+            <p class="username">
+                <input name="username" type="username" value="" data-validation="nonempty username" id="username" placeholder="username" />
+            </p>
+            <p class="text">
+                <input name="password" type="password" data-validation="nonempty" id="password" placeholder="Password" />
+            </p>
+            <div class="submit">
+                <button type="submit">Login</button>
+            </div>
+        </form>
+    </div>
 @endif
