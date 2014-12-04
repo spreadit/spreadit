d1a8f73638070db6ac8b2466ea9b422589b129a4
Jett <whackashoe@gmail.com>
2014-12-04 00:10:17 -0600

    add link to homepage on profile

--- app/views/page/user/profile.blade.php
+++ app/views/page/user/profile.blade.php
@@ -9,4 +9,6 @@
 
 @section('content')
 {{ $username }} is {{ ($stats->anonymous) ? 'an anonymous' : 'an ominous' }} {{ $stats->achievement }}<sup>(lvl {{ $stats->level }})</sup> spawned in the year {{ date("Y", $stats->created_at) }} on the glorious day of {{ date("F", $stats->created_at) }} the {{ date("jS", $stats->created_at) }}
+<br><br>
+visit {{ $username }}'s homepage <a href="http://{{ $username }}.spreadit.io">here</a>
 @stop
